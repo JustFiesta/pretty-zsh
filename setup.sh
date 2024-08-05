058533d63a19b95443752bc206006d63094b5892
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-08-05 14:58:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # Get autonsuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Get autocomplete plugin
-mkdir -p $HOME/.oh-my-zsh/custom/plugins/
-git clone https://github.com/larkery/zsh-histdb $HOME/.oh-my-zsh/custom/plugins/zsh-histdb
+# Get syntax highiting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Open new zsh session
 exec zsh
